cb79cac7039e1b1c2a596f1078515cedbb47ea43
Jude <105988359+JudeAssaeedi@users.noreply.github.com>
2024-10-01 20:48:44 +0300

    Initial commit

---
+++ Tetris.py
@@ -0,0 +1,51 @@
+import pygame, sys
+from game import Game
+
+pygame.init() 
+dark_blue = (23, 8, 87)
+
+screen = pygame.display.set_mode((530 ,620)) #350 = width 600 = height
+pygame.display.set_caption("Testris")
+
+clock = pygame.time.Clock() #how fast the game will run (frame rate)
+
+game = Game()
+
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 200)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if game.game_over == True:
+                game.game_over = False
+                game.reset()
+            if event.key == pygame.K_LEFT and game.game_over == False:
+                game.move_left()
+            if event.key == pygame.K_RIGHT and game.game_over == False:
+                game.move_right()
+            if event.key == pygame.K_DOWN and game.game_over == False:
+                game.move_down()
+            if event.key == pygame.K_UP and game.game_over == False:
+                game.rotate()
+            if event.key == pygame.K_a and game.game_over == False:
+                game.move_left()
+            if event.key == pygame.K_d and game.game_over == False:
+                game.move_right()
+            if event.key == pygame.K_s and game.game_over == False:
+                game.move_down()
+            if event.key == pygame.K_w and game.game_over == False:
+                game.rotate()
+        if event.type == GAME_UPDATE and game.game_over == False:
+            game.move_down()
+            
+
+    #Drawing
+    screen.fill(dark_blue) #background color
+    game.draw(screen)
+
+    pygame.display.update() #update screen
+    clock.tick(60) #the game will run at 60 frames per secondSddd
---
+++ colors.py
@@ -0,0 +1,15 @@
+class Colors: 
+        dark_grey = (26, 31, 40)
+        green = (47, 230, 23)
+        red = (232, 18, 18)
+        orange = (226, 116, 17)
+        white = (225, 225, 225)
+        yellow = (237, 234, 4)
+        purple = (166, 0, 247)
+        cyan = (21, 204, 209)
+        dark_green = (10, 40, 15)
+        blue = (13, 64, 225)
+
+        @classmethod
+        def get_cell_colors(cls):
+                return [cls.dark_grey, cls.green, cls.red, cls.orange, cls.yellow, cls.white, cls.purple, cls.cyan, cls.dark_green, cls.blue]
